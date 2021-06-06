b58573f9281c979a2378a1ce9e83b04a8b7374d7
Rajeev Shukla <rajeevshukla255@gmail.com>
2021-06-06 21:01:30 +0530

    Pin model added

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
-
-
 require('dotenv').config()
+
+
+app.use(express.json())
+//DATABASE
+
+mongoose 
+ .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,   })   
+ .then(() => console.log("MongoDB connected!"))
+ .catch(err => console.log(err));
 
 //PORT
 const PORT = process.env.PORT || 4000
